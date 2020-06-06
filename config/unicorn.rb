cb22460f75aed767c9af4bc073b924a66b2fb55b
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-06 13:28:27 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 2
@@ -8,7 +8,7 @@ worker_processes 2
 working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/uni.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen 3000
