d9fabd18fe3002a3e6f03d35dd70b50f84d18901
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-06 21:40:29 +0900

    make_seed
    ## what
    - エラー修正
    
    ## why
    - エラー修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 
 Song.create(id:1, title: "Little Maggie", jam: 0, standard:1, beginner:0, user_id:1)
-Chords.create(id: 1 song_id: 1, artist_id: 1, album_id:1, version: "traditional_version")
+Chord.create(id: 1, song_id: 1, artist_id: 1, album_id:1, version: "traditional_version")
