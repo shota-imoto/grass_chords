eb007fa26ff78a3fbe78e7ed8b916b74d1e22b88
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-06 13:22:25 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 2
