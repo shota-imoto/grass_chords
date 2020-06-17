90ff5b858be78bbf593aa210d268d8775873dc35
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-17 21:35:34 +0900

    adjust_user_create_function
    ## what
    - ユーザー登録機能の修正
    
    ## why
    - ユーザー個別サービスを後に追加するためのベース機能として実装

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -9,9 +9,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -23,5 +23,13 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  # def after_sign_in_path_for(resource)
+  #   root_path(resource)
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   root_path(resource)
   # end
 end
