42413437784cbd0e2b4c4c7abb0eb214b30232dc
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-21 16:03:47 +0900

    refactoring_1
    ## what
    - cssファイル全般の修正
    - css記述の削除
    
    ## why
    リファクタリング1
    - 旧フロントデザインに適用されていたjs, css記述を削除
    - scaffoldで作成されたが使用していないファイルを削除
    - flocss記述が適用されていないcss名称を修正

--- app/assets/javascripts/navi.js
+++ app/assets/javascripts/navi.js
@@ -80,37 +80,3 @@ $(function () {
     $(this).find(".p-navi__icon-box").eq(1).toggleClass("c-upside-down");
   });
 });
-
-// old
-
-// $(function () {
-
-//   $(".navi-menu").on("touchend, mouseup", function () {
-//     if ($("#navi").hasClass("open")) {
-//       // メニューがプルアップする
-//       $("#navi").removeClass("open");
-//       $("#navi").addClass("close");
-
-//       $(".navi-menu span").eq(0).removeClass("open-top");
-//       $(".navi-menu span").eq(1).removeClass("open-middle");
-//       $(".navi-menu span").eq(2).removeClass("open-bottom");
-
-//       $(".navi-menu span").eq(0).addClass("close-top");
-//       $(".navi-menu span").eq(1).addClass("close-middle");
-//       $(".navi-menu span").eq(2).addClass("close-bottom");
-//     } else {
-//       // メニューがプルダウンする
-//       $("#navi").addClass("open");
-//       $("#navi").removeClass("close");
-//       $("#navi").show();
-
-//       $(".navi-menu span").eq(0).removeClass("close-top");
-//       $(".navi-menu span").eq(1).removeClass("close-middle");
-//       $(".navi-menu span").eq(2).removeClass("close-bottom");
-
-//       $(".navi-menu span").eq(0).addClass("open-top");
-//       $(".navi-menu span").eq(1).addClass("open-middle");
-//       $(".navi-menu span").eq(2).addClass("open-bottom");
-//     }
-//   });
-// });
