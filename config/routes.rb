f9e4a0b68a4d799c50383b07a026a27f60afff15
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-06-22 06:23:57 +0900

    delete_routing-conf_non-using
    ## what
    - route.rbの記述を削除
    
    ## why
    - 未使用ルーティングにかかる記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,12 @@
 Rails.application.routes.draw do
   root to: "chords#index"
-  resources :pdf, only: :new
   devise_for :users, except: :index
   resources :songs do
     collection do
       get :search
-      get :search_page
-      get :search_detail
     end
   end
   resources :chords, except: :index
   resources :likes, only: :create
-  resources :scores
   resources :practices, except: [:new, :edit, :show]
-  resources :instruments, except: :index do
-    collection do
-      get :search
-    end
-  end
-  resources :keys, only: :create
-  resources :tuning_alls, except: :show do
-  end
-  resources :tuning_all_form, only: [:new, :create]
-  resources :finger_alls, except: :show
 end
