470905bbb66699fc3ef400bc7dffe88324251407
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-07-03 20:37:00 +0900

    design_sort-btn
    ## what
    - sort.jsにて、画面更新時とsortボタン選択時にチェック状態を確認して着色を切り替える機能を実装
    - cssにてsortボタンのレイアウトおよびデザインを定義
    
    ## why
    - sortボタンのオン・オフを着色で表現することにより、直感的に現在のソート条件を把握することができる

---
+++ app/assets/javascripts/sort.js
@@ -0,0 +1,24 @@
+// c-js__like
+$(function () {
+  // when user selects btn, make btn colored
+  $(".p-search-result__sort-btn").on("touchend, mouseup", function () {
+    change_sort_color($(this));
+  });
+
+  // when page reloads, make btn colored
+  $(document).ready(function () {
+    var btn = $("input[name= 'sort']:checked").parent();
+    change_sort_color($(btn));
+  });
+
+  function change_sort_color(btn) {
+    // if selected-btn isn't colored
+    if ($(btn).hasClass("u-js__sort-color") == false) {
+      $(".p-search-result__sort-btns")
+        .find(".u-js__sort-color")
+        .removeClass("u-js__sort-color");
+      $(btn).addClass("u-js__sort-color");
+    } else {
+    }
+  }
+});
