27352c2f15494698eaa40edd54b8b0491017d073
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-07-09 07:30:54 +0900

    fix_syntax_error
    ## what
    - config/applicationの記述を修正
    
    ## why
    - シンタックスエラーの修正

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module GrassChords
       g.test_framework :rspec,
         fixtures: false,
         view_specs: false,
-        helper_specs: false.
+        helper_specs: false,
         routing_specs: false
     end
 
